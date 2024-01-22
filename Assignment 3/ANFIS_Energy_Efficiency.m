e75b9f314e45605bcb3b7ba3f4ff7b2a1b3e358d
Mario Rosas <mario.rosas@sopristec.com>
2024-01-22 19:31:11 +0100

    Adding code of ANFIS

---
+++ Assignment 3/ANFIS_Energy_Efficiency.m
@@ -0,0 +1,40 @@
+% Load the dataset from an Excel file
+data = readtable('energy_efficiency_data.xlsx');
+
+% Assuming the first 8 columns are input features and the 9th column is the target (heating load)
+inputs = table2array(data(:, 1:8));
+target = table2array(data(:, 9));
+
+% Assuming inputs is your dataset
+meanVals = mean(inputs);
+stdVals = std(inputs);
+
+% Standardize data
+inputs_standardized = (inputs - meanVals) ./ stdVals;
+
+%% Split data into training and testing sets
+cv = cvpartition(size(data,1),'HoldOut',0.2); % for example, 80% training, 20% testing
+idx = cv.test;
+
+% Separate the training and testing data
+inputs_train = inputs_standardized(~idx,:);
+target_train = target(~idx,:);
+inputs_test = inputs_standardized(idx,:);
+target_test = target(idx,:);
+
+%% Generate an initial FIS structure
+%optGF = genfisOptions("SubtractiveClustering");
+optGF = genfisOptions('FCMClustering','FISType','sugeno');
+%optGF.NumMembershipFunctions = 3;
+%optGF.InputMembershipFunctionType = "gbellmf";
+fis = genfis(inputs_train, target_train, optGF);
+
+%% Train the ANFIS model
+numEpochs = 3; % Number of epochs for training
+[trainedFis,trainError] = anfis([inputs_train target_train], fis, numEpochs);
+%% Predict the heating load for the test set
+predicted = evalfis(trainedFis, inputs_test);
+
+% Calculate Mean Absolute Error (MAE)
+mae = mean(abs(predicted - target_test));
+disp(['Mean Absolute Error: ', num2str(mae)]);
---
+++ Assignment 3/K_Fold_ANFIS_Energy_Efficiency.m
@@ -0,0 +1,57 @@
+% Load the dataset from an Excel file
+data = readtable('energy_efficiency_data.xlsx');
+
+% Assuming the first 8 columns are input features and the 9th column is the target (heating load)
+inputs = table2array(data(:, 1:8));
+target = table2array(data(:, 9));
+
+% Normalize the inputs
+meanVals = mean(inputs);
+stdVals = std(inputs);
+inputs_normalized = (inputs - meanVals) ./ stdVals;
+
+% Initialize cross-validation
+cv = cvpartition(size(data,1),'KFold',3);
+
+% Initialize vectors to store metrics for each fold
+mae_values = zeros(cv.NumTestSets,1);
+mse_values = zeros(cv.NumTestSets,1);
+mre_values = zeros(cv.NumTestSets,1);
+
+for i = 1:cv.NumTestSets
+    % Indices for training and test set
+    trainIdx = training(cv, i);
+    testIdx = test(cv, i);
+    
+    % Splitting the data
+    inputs_train = inputs_normalized(trainIdx,:);
+    target_train = target(trainIdx);
+    inputs_test = inputs_normalized(testIdx,:);
+    target_test = target(testIdx);
+    
+    % Generate an initial FIS structure
+    optGF = genfisOptions('FCMClustering','FISType','sugeno');
+    fis = genfis(inputs_train, target_train, optGF);
+    
+    % Train the ANFIS model
+    numEpochs = 3; % Adjust as needed
+    [trainedFis, trainError] = anfis([inputs_train target_train], fis, numEpochs);
+    
+    % Predict the heating load for the test set
+    predicted = evalfis(trainedFis, inputs_test);
+    
+    % Calculate Mean Absolute Error (MAE)
+    mae_values(i) = mean(abs(predicted - target_test));
+    
+    % Calculate Mean Squared Error (MSE)
+    mse_values(i) = mean((predicted - target_test).^2);
+    
+    % Calculate Mean Relative Error (MRE)
+    % Note: Add a small number to avoid division by zero
+    mre_values(i) = mean(abs((predicted - target_test) ./ (target_test + eps)));
+end
+
+% Display average of the metrics
+disp(['Average MAE: ', num2str(mean(mae_values))]);
+disp(['Average MSE: ', num2str(mean(mse_values))]);
+disp(['Average MRE: ', num2str(mean(mre_values))]);
